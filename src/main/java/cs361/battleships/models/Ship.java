2e862c3934052652657454a1ce1b41810dbbb1c3
Zachary Horine <horine.zachary@gmail.com>
2018-10-10 16:40:02 -0700

    add ship constructor

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -7,10 +7,33 @@ import java.util.ArrayList;
 
 public class Ship {
 
+    int length;
+    String shipType;
+
 	@JsonProperty private List<Square> occupiedSquares;
 	
 	public Ship(String kind) {
-		//TODO implement
+		switch(kind) {
+            case "Carrier":
+                length = 5;
+                break;
+            case "Battleship":
+                length = 4;
+                break;
+            case "Cruiser":
+                length = 3;
+                break;
+            case "Submarine":
+                length = 3;
+                break;
+            case "Destroyer":
+                length = 2;
+                break;
+            default:
+                length = 0;
+                break;
+        }
+        shipType = kind;
 		occupiedSquares = new ArrayList<>();
 	}
 
