4a47bc4fec253e370fe085955d7f52f5587bea37
Zachary Horine <horine.zachary@gmail.com>
2018-10-10 23:17:25 -0700

    Refactor check for occupied squares, integrate check into if statement, with short circuit

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -33,7 +33,7 @@ public class Board {
         List<Square> shipsquares = new ArrayList<Square>();
 		for (int i = 0; i < ship.getLength(); i++){
 		    if (isVertical){
-		        if (this.isSquareEmpty(x-1+i, ((int)y)-65) && (x-1+i < BOARDSIZE_X) && (((int)y)-65) < BOARDSIZE_Y) {
+		        if ((x-1+i < BOARDSIZE_X) && (((int)y)-65) < BOARDSIZE_Y && !boardarray[x-1+i][((int)y)-65].getOccupied()) {
                     System.out.println(x-1+","+ (((int)y)-65+i));
                     shipsquares.add(boardarray[x-1+i][((int)y)-65]);
 			        boardarray[x-1+i][((int)y)-65].setOccupied(true);
@@ -41,7 +41,7 @@ public class Board {
                 else return false;
             }
             else{
-                if (this.isSquareEmpty(x-1, ((int)y)-65+i) && (x-1+i < BOARDSIZE_X) && (((int)y)-65+i) < BOARDSIZE_Y) {
+                if ((x-1+i < BOARDSIZE_X) && (((int)y)-65+i) < BOARDSIZE_Y && !boardarray[x-1][((int)y)-65+i].getOccupied()) {
                     System.out.println(x-1+","+ (((int)y)-65+i));
                     shipsquares.add(boardarray[x-1][((int)y)-65+i]);
                     boardarray[x-1][((int)y)-65+i].setOccupied(true);
@@ -84,24 +84,11 @@ public class Board {
 		//TODO implement
 	}
 
-	public boolean isSquareEmpty (int x, int y){
-		/*
-        for (int i = 0; i < ships.size(); i++){
-            for (int j = 0; j < ships.get(i).getOccupiedSquares().size(); j++) {
-                if(ships.get(i).getOccupiedSquares().get(j).getColumn() == (char)(y+65)){return false;}
-                if(ships.get(i).getOccupiedSquares().get(j).getRow() == (x+1)){return false;}
-            }
-        }
-	    return true;
-        */
-		return !boardarray[x][y].getOccupied();
-    }
-
     private void printBoard (){
 	    for (int i = 0; i < boardarray.length;i++){
 	        System.out.print("|");
 	        for (int j = 0; j < boardarray[0].length; j++){
-                if(isSquareEmpty(i,j)){System.out.print("O|");}
+                if(!boardarray[i][j].getOccupied()){System.out.print("O|");}
                 else{System.out.print("X|");}
             }
             System.out.print("\n");
