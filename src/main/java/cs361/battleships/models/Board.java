c1b5083279d64e00b617132a308b64196eb6007f
Zachary Horine <horine.zachary@gmail.com>
2018-10-11 00:58:21 -0700

    Remove printboard

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Board {
     private Square[][] boardarray;
-    private List<Ship> ships;
 
     final int BOARDSIZE_X = 10;
     final int BOARDSIZE_Y = 10;
@@ -28,7 +27,7 @@ public class Board {
         }
         ships = new ArrayList<Ship>();
     	attacks = new ArrayList<>();
-		printBoard();
+		//printBoard();
 	}
 
 	/*
