7b4af915f684b0c5d941875181135d86a3472031
Michael Huang <sleep3030@gmail.com>
2018-10-24 21:52:04 -0700

    Removed some stuff, still has grey square issue.

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -2,7 +2,7 @@ var isSetup = true;
 var placedShips = 0;
 var game;
 var shipType;
-var vertical;
+var vertical = false;
 
 function makeGrid(table, isPlayer) {
     for (i=0; i<10; i++) {
@@ -67,7 +67,6 @@ function cellClick() {
     let col = String.fromCharCode(this.cellIndex + 65);
     console.log(col);
     console.log(row);
-    document.getElementById("output").innerHTML = col;
     if (isSetup) {
         sendXhr("POST", "/place", {game: game, shipType: shipType, x: row, y: col, isVertical: vertical}, function(data) {
             game = data;
@@ -109,25 +108,21 @@ function verticalButton(e){
     /*V keycode 86*/
 /*}*/
  document.addEventListener("keydown", function(event) {
-            console.log("V was pressed");
-            var x = event.keyCode;
-            if (x == 86 && vertical == false){
-                console.log("Test");
+            var key = event.keyCode;
+            if (key == 86 && vertical == false){
                 vertical = true;
             }
-            else if (x == 86 && vertical == true){
+            else if (key == 86 && vertical == true){
                 vertical = false;
             }
-            else{
-                vertical = false;
-            }
+
         });
 
 function place(size) {
     return function() {
         let row = this.parentNode.rowIndex;
         let col = this.cellIndex;
-        document.addEventListener
+
        /* vertical = false; /*document.getElementById("is_vertical").checked;*/
 
         let table = document.getElementById("player");
