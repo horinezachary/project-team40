db8d8e63cce240704f7bb348485e7114735db487
Benjamin Wilson Friedman <friedman.benjamin@gmail.com>
2018-11-08 21:09:28 -0800

    Styling for the captain's quarters (#101)

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -123,6 +123,9 @@ function redrawGrid() {
 
     game.playersBoard.ships.forEach((ship) => ship.occupiedSquares.forEach((square) => {
         document.getElementById("player").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied");
+        if(square.captain) {
+            document.getElementById("player").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied-captain");
+        }
     }));
     markHits(game.opponentsBoard, "opponent", "You won the game");
     markHits(game.playersBoard, "player", "You lost the game");
