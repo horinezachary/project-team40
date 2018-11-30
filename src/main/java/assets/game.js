9309213e2f647c1223d9d79bc5f85a30241dae18
Michael Huang <sleep3030@gmail.com>
2018-11-30 11:08:02 -0800

    Added a bit more text

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -253,7 +253,7 @@ function cellClick() {
                     ss.title = "Click here and select a space to perform a sonar pulse.";
                     ss.childNodes[0].src = "/assets/images/sweep2.png";
                     ss.childNodes[0].className = "animate-this";
-                    (new Toast("Sonar Pulse Available!", "#0f0")).show();
+                    (new Toast("Sonar Pulse and Laser Weapon Enabled!", "#0f0")).show();
 
                     var LS = document.getElementById("LaserWeapon");
                     LS.title = "ION Cannon Available";
